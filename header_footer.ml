eba6ed037f7491c96b8a56fc5fed6f8f434776e5
qwer@gnuBrian <brian@jonnes.co.za>
2021-11-19 20:09:20 +1300

    Initial Commit.

---
+++ header_footer.ml
@@ -0,0 +1,40 @@
+<record id='nav-date-prefix'></record><record id='nav-date-suffix'></record><record id=onload><script>function ktwkc_onload(){}</script></record><record id="header_footer"><!doctype html><module api path="kktw.php"><html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="w3css4.css"/>
+	<!-- link rel="icon" href="/icon.png"/ -->
+	<title><play id="title"/></title>
+	<play id="meta"/>
+	<include path='ktwkc.cssml'/>
+	<include path='style.cssml'/>
+	<include path='theme.cssml'/>
+	<script language="javascript">
+	if(navigator.share) {
+		document.write('\x3cstyle type="text/css">');
+		document.write('.js_no_share { display: none; }');
+		document.write('span.js_with_share { display: inline; }');
+		document.write('li.js_with_share { display: list-item; }');
+		document.write('\x3c/style>');
+	}
+	</script>
+	<play id=onload/>
+  </head>
+<body onload="ktwkc_onload()" class="w3-theme-l5"><div class=ktwkc-b><!-- Aa -->
+
+<sailing><div class="w3-tiny w3-black" style="text-align:right;background-color:#010101!important;padding:0 16px">Obligatory "ignore this space" : <a class="w3-black" style="background-color:#010101!important;text-decoration:none" href="https://sacoronavirus.co.za">https://sacoronavirus.co.za</a></div></sailing>
+
+<div class="ktwkc-Aa"><button class="a" id="myBtn" onclick="var x=document.body.childNodes[0];x.className=x.className.substring(0,6)+String.fromCharCode((x.className.charCodeAt(6)-97+1)%3+97)">Aa</button></div>
+
+<!--<div class="w3-container w3-theme-l4">
+<p>For you, with compliments, a link from the Internet:<random><a href="https://coughupcovid.ca/story">The Ultimate Fairytale : A short history of Greece, Rome, Israel, England, France, and Germany.</a></random></p>-->
+</div>
+<include path="nav.ml"/>
+
+	<play id="content"/>
+
+<div class="w3-container w3-theme-l5 w3-clear">
+<div class=w3-right><!--span class="js_with_share"><a href="#" onclick="if(navigator.share)navigator.share({title:document.title,url:location.href});return false">Share</a--> <span>&bull;</span><!--last_mod_southern/--></div>
+</div>
+
+</body>
+</html></module></record>
---
+++ nav.ml
@@ -0,0 +1,13 @@
+<div class="w3-bar w3-theme-l3 w3-border-bottom w3-border-top w3-border-theme nav">
+<div class="ab">
+<!--<time-progress/> -->
+	<button class="w3-bar-item w3-button w3-show-large" onclick="parentNode.className=parentNode.className.substring(1)+parentNode.className.substring(0,1);">|:|</button>
+	<span class="w3-bar-item version" href="/changelog"><play id='nav-date-prefix'/><a href='/'><date minus=0 plus=0/><day minus=0 plus=0/></a><play id='nav-date-suffix'/><!--play id="version"/--></span>
+	<div class="w3-bar-item"><a class=home href="/">K<small>KTW</small>&#8482;</a></div>
+<!--	<a.site class="w3-bar-item w3-button" activeclass="active" href="/brianjonnes">&#169;BJ</a.site>
+	<a.site class="w3-bar-item w3-button" activeclass="active" href="/random">&amp;opinion;</a.site>
+    <a.site class="w3-bar-item w3-button" activeclass="active" href="/load_shedding"><small>LOAD-SHEDDING-JHB</small></a.site>
+    <a.site class="w3-bar-item w3-button" activeclass="active" href="/howto">&lt;howww<sub>2</sub>/></a.site>-->
+	<div class=extra><play id="nav-extra"/></div>
+</div>
+</div>
