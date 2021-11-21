9dde2d16a6cee4841f3b399eb8c633153dc20225
brian@demicole <brian@jonnes.co.za>
2021-11-22 00:54:54 +0200

    Amendments.

--- nav.ml
+++ nav.ml
@@ -1,9 +1,8 @@
 <div class="w3-bar w3-theme-l3 w3-border-bottom w3-border-top w3-border-theme nav">
 <div class="ab">
-<!--<time-progress/> -->
 	<button class="w3-bar-item w3-button w3-show-large" onclick="parentNode.className=parentNode.className.substring(1)+parentNode.className.substring(0,1);">|:|</button>
 	<span class="w3-bar-item version"><eggsgml_version decimal=,/>/<eggsgml_api_version/></span>
-	<div class="w3-bar-item"><a class=home href="/">K<small>KTW-LUNCHBOX</small>&#8482;</a></div>
+	<div class="w3-bar-item"><a.site class=home href="/">K<small>KTW-LUNCHBOX</small>&#8482;</a.site></div>
 	<a.site class="w3-bar-item w3-button" activeclass="active" href="/getting-started"><small>GETTING STARTED</small></a.site>
 <!--	<a.site class="w3-bar-item w3-button" activeclass="active" href="/random">&amp;opinion;</a.site>
     <a.site class="w3-bar-item w3-button" activeclass="active" href="/load_shedding"><small>LOAD-SHEDDING-JHB</small></a.site>
