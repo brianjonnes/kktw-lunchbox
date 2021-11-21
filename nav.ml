1eaed9f31130c7c59bb0d8dba799478b934278a8
qwer@gnuBrian <brian@jonnes.co.za>
2021-11-22 11:58:15 +1300

    Amendments.

--- nav.ml
+++ nav.ml
@@ -2,7 +2,7 @@
 <div class="ab">
 	<button class="w3-bar-item w3-button w3-show-large" onclick="parentNode.className=parentNode.className.substring(1)+parentNode.className.substring(0,1);">|:|</button>
 	<span class="w3-bar-item version"><eggsgml_version decimal=,/>/<eggsgml_api_version/></span>
-	<div class="w3-bar-item"><a.site class=home href="/">K<small>KTW-LUNCHBOX</small>&#8482;</a.site></div>
+	<div class="w3-bar-item"><a.site class=home activeclass=active href="/">K<small>KTW-LUNCHBOX</small>&#8482;</a.site></div>
 	<a.site class="w3-bar-item w3-button" activeclass="active" href="/getting-started"><small>GETTING STARTED</small></a.site>
 <!--	<a.site class="w3-bar-item w3-button" activeclass="active" href="/random">&amp;opinion;</a.site>
     <a.site class="w3-bar-item w3-button" activeclass="active" href="/load_shedding"><small>LOAD-SHEDDING-JHB</small></a.site>
