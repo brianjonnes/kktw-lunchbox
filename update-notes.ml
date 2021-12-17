6293398e0c90e1e346772a7adf4aa0ee2758fbcb
brian@demicole <brian@jonnes.co.za>
2021-12-17 19:14:09 +0200

    Amendments.

---
+++ update-notes.ml
@@ -0,0 +1,16 @@
+
+<table>
+<tr><td>5/1</td><td>Optional (downgrade from recommendation)</td></tr>
+<tr><td></td><td>Place shipyard auth code into templates_config.xml 
+(see shipyard-auth in the reference).</td></tr>
+<tr><td></td><td>Remove the contents of shipyard.txt (the empty
+file must remain).</td></tr>
+</table>
+
+<table>
+<tr><td>6/2</td><td>Entry code</td></tr>
+<tr><td></td><td>Custom templates.php requires modules.php (enqueue_modules).</td></tr>
+<tr><td>6/1</td><td>Cleanup</td></tr>
+<tr><td></td><td>Remove no-doctype from templates_config.xml.</td></tr>
+</table>
+
